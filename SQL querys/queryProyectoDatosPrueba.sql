871dd6f4cf0eed68831d5480733ddf724eacecb6
Aldo Soria Varela <111372099+aldosova@users.noreply.github.com>
2023-03-06 23:42:16 -0600

    Create queryProyectoDatosPrueba.sql

---
+++ SQL querys/queryProyectoDatosPrueba.sql
@@ -0,0 +1,5 @@
+insert into Estado values(1, 'CDMX')
+insert into Ciudad values(1, 'CDMX',1)
+
+insert into Voluntario values(1,'juan@gmail.com','juanito','Juan Gonzalez',22,'H',512345678,'IODSNIFODISNFOIODS',1)
+insert into Organizacion values(1,'ayudamx@gmail.com','ayuditamx','Ayuda Mexico', 513325235, 'Rio Hondo 12',1) 
