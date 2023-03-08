a5fe60420ec352156399a4c3e42c2dff28b789e5
Aldo Soria Varela <111372099+aldosova@users.noreply.github.com>
2023-03-07 20:30:27 -0600

    Update queryProyectoSelects.sql

--- SQL querys/queryProyectoSelects.sql
+++ SQL querys/queryProyectoSelects.sql
@@ -2,3 +2,4 @@ use FuerzaR
 
 select ClaveVoluntario, Nombre from Voluntario where Correo = 'juan@gmail.com' and Contrasena = 'juanito'
 select ClaveOrganizacion, Nombre from Organizacion where Correo = 'ayudamx@gmail.com' and Contrasena = 'ayuditamx'
+select ClaveAdministrador, Nombre from Administrador where Correo = 'jorge@gmail.com' and Contrasena = 'jorgito'
