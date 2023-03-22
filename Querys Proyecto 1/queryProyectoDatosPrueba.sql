394bb845cb5ca67e546b05736ca9aca6f3d029b9
Aldo Soria Varela <111372099+aldosova@users.noreply.github.com>
2023-03-21 22:03:04 -0600

    Delete Querys Proyecto 1 directory

--- Querys Proyecto 1/queryProyectoDatosPrueba.sql
+++
@@ -1,9 +0,0 @@
-use FuerzaR
-
-insert into Estado values(1, 'CDMX')
-insert into Ciudad values(1, 'CDMX',1)
-
-insert into Voluntario values(1,'juan@gmail.com','juanito','Juan Gonzalez',22,'H',512345678,'IODSNIFODISNFOIODS',1)
-insert into Organizacion values(1,'ayudamx@gmail.com','ayuditamx','Ayuda Mexico', 513325235, 'Rio Hondo 12',1) 
-
-insert into Administrador values(1,'Jorge Perez','jorge@gmail.com','jorgito')
